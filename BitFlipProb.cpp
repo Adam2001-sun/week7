b0c07fce0f437b0078c57777baa1d781b12e5647
Adam2001-sun <83719329+Adam2001-sun@users.noreply.github.com>
2022-05-02 19:52:28 +0930

    Add files via upload

--- BitFlipProb.cpp
+++ BitFlipProb.cpp
@@ -5,20 +5,22 @@
 #include "BitFlipProb.h"
 
  BitFlipProb::BitFlipProb() = default;
- 
+
 Individual *BitFlipProb::mutate(Individual * individual, int k) {
-    //reference from https://stackoverflow.com/questions/39288595/why-not-just-use-random-device
-    std::random_device rd;
-    std::mt19937 e{rd()};
-    std::uniform_int_distribution<double> dist{0, 1};
+    // //reference from https://stackoverflow.com/questions/39288595/why-not-just-use-random-device
+    // std::random_device rd;
+    // std::mt19937 e{rd()};
+    // std::uniform_int_distribution<double> dist{0, 1};
 
 // get random numbers with:
-    dist(e);
+    // dist(e);
+
+int randnum = rand()%2;
 
     std::string tmp_string = individual->getString();
 
     for (char &c: tmp_string) {
-        double current_num = dist(e);
+        double current_num = randnum;
 
         if (current_num <= this->prob) {
             c = c == '0' ? '1' : '0';
